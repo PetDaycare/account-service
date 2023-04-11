e19cb947e9d836e4de07d18d495886f4dd72b03b
roboter5123 <pascal.roboter@hotmail.com>
2023-04-11 21:10:20 +0200

    i fucked up please work now

--- src/main/java/com/petdaycare/userservice/model/dto/LoginSessionDTO.java
+++ src/main/java/com/petdaycare/userservice/model/dto/LoginSessionDTO.java
@@ -1,5 +1,6 @@
 package com.petdaycare.userservice.model.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.petdaycare.userservice.model.LoginSession;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +22,10 @@ public class LoginSessionDTO {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime expires;
 
+    public LoginSessionDTO(LoginSession session) {
 
+        this.userId = session.getUser().getUserId();
+        this.loginToken = session.getLoginToken();
+        this.expires = session.getExpires();
+    }
 }
