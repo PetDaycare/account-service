d659c2f5a3393cac030359f2f97f13e685cbc258
roboter5123 <pascal.roboter@hotmail.com>
2023-04-18 18:03:35 +0200

    Rdeprecated classes

--- src/main/java/com/userservice/exception/GenericConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.userservice.exception;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
-public class GenericConflictException extends RuntimeException {
-
-}
